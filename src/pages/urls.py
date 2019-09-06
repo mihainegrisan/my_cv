5c328150a62a98eef691cf4582a91699d59c2236
mihai.negrisan <mihainegrisan1@gmail.com>
2019-09-06 21:56:18 +0300

    Added a burger menu in mobile view which can be toggled.

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from pages.views import home_view, about_view, contact_view, survey_view, weather_view, rock_paper_scissors_view
+from pages.views import (home_view,
+                         about_view,
+                         contact_view,
+                         survey_view,
+                         weather_view,
+                         rock_paper_scissors_view,
+                         music_app_view,
+                         page_animation_view)
 
 app_name = 'pages'
 
@@ -12,5 +18,8 @@ urlpatterns = [
     path('survey/', survey_view, name='survey'),
     path('weather/', weather_view, name='weather'),
     path('rock_paper_scissors/', rock_paper_scissors_view, name='rock_paper_scissors'),
+    path('music_app/', music_app_view, name='music_app'),
+    path('page_animation/', page_animation_view, name='page_animation'),
+
 
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -30,3 +30,14 @@ def rock_paper_scissors_view(request, *args, **kwargs):
 
     context = {'section' : 'rock_paper_scissors'}
     return render(request, 'pages/rock_paper_scissors.html', context)
+
+
+def music_app_view(request, *args, **kwargs):
+
+    context = {'section' : 'music_app'}
+    return render(request, 'pages/music_app.html', context)
+
+def page_animation_view(request, *args, **kwargs):
+
+    context = {'section' : 'page_animation'}
+    return render(request, 'pages/page_animation.html', context)
